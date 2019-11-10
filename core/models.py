427a3aa17134ac9515cf3d876681d0624042e357
Roman <r9052933642@gmail.com>
2019-11-10 15:29:09 +0300

    create Item and Category model, add to admin page

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=150, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    category = models.ManyToManyField(Category)
+    count = models.PositiveIntegerField()
+    description = models.TextField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.title
