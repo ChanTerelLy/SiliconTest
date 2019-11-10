64a9bf7faa37e8d7eab521a40ee6b300cf017ea0
Roman <r9052933642@gmail.com>
2019-11-10 23:49:10 +0300

    minor change

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'wsv8qeaegvil18m&vmfeob^aa7wy74c8w4@0p6ro%)z0yi3^fm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
